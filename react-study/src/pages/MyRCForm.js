854fc48c77cb8b4ae503999a4e3d11318039c3d9
randy0423 <1156107187@qq.com>
2020-08-28 07:55:00 +0800

    表单

---
+++ react-study/src/pages/MyRCForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyRCForm extends Component {
+	render() {
+		return (
+			<div>
+				<h3>MyRCForm</h3>
+
+				
+			</div>
+		);
+	}
+}
+
+export default MyRCForm;

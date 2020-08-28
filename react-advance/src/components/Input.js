691018480649d6c478cac6da18121c0d32249872
RandyQin <randyqin@haizol.com>
2020-08-28 11:59:16 +0800

    rc-form

--- react-advance/src/components/Input.js
+++ react-advance/src/components/Input.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
 
-
-export default class CustomizeInput extends Component {
+class CustomizeInput extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
   render() {
-    // 接收传入的属性
-    const {value ='' , ...inputprops} = this.props
+    const {value = "", ...otherProps} = this.props;
     return (
       <div style={{padding: 10}}>
-        <Input value={value} {...inputprops}/>
+        <Input style={{outline: "none"}} value={value} {...otherProps} />
       </div>
-    )
+    );
   }
 }
 
-// 输入框
+
 function Input(props) {
   return (
-    <input {...props}/>
+    <input {...props} />
   )
 }
+
+
+export default CustomizeInput;
--- react-advance/src/pages/MyRCForm.js
+++ react-advance/src/pages/MyRCForm.js
@@ -1,28 +1,50 @@
+/* eslint-disable no-useless-constructor */
 import React, { Component } from 'react'
+import createForm from "../components/my-rc-form/";
 import Input from '../components/Input'
-import createForm from '../components/my-rc-form/index'
 
-@createForm()
+// 暗号：贝宁
+const nameRules = {required: true, message: "请输入姓名！"};
+const passworRules = {required: true, message: "请输入密码！"};
+
+// 高阶组件
+@createForm
 class MyRCForm extends Component {
   constructor(props) {
     super(props)
-    // 弊端：需要自己定义state, input 需要自己定义onchange事件
-    this.state = {
-      username: '',
-      password: ''
-    }
   }
   submit = () => {
-    console.log('submit', this.state)
+    // 获取getvalue ,和校验的方法
+    const {getFieldsValue, validateFields} = this.props.form
+    console.log('submit', getFieldsValue())
+    // 校验
+    validateFields((err, val) => {
+      if (err) {
+        console.log("err", err);
+      } else {
+        console.log("校验成功", val); 
+      }
+    });
+  }
+  componentDidMount() {
+    // 设置初始值
+    this.props.form.setFieldsValue({username: 'aaa'})
   }
   render() {
+    console.log("props", this.props)
+    // 获取存储数据方法
+    const {getFieldDecorator} = this.props.form;
     return (
       <div>
         <h3>MyRCForm</h3>
         {/* <Input placeholder="Username" value={username} onChange={ e => this.setState({username: e.target.value})}/>
         <Input placeholder="Password" value={password} onChange={ e => this.setState({password: e.target.value})}/> */}
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
+        {
+          getFieldDecorator('username', {rules: [nameRules]})(<Input placeholder="Username" />)
+        }
+        {
+          getFieldDecorator('password', {rules: [passworRules]})(<Input placeholder="Password" />)
+        }
         <button onClick={this.submit}>submit</button>
       </div>
     )

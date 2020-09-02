d1cafabcfa5dfb938d4735d6698b968215b5b4c8
randy0423 <1156107187@qq.com>
2020-09-03 07:38:35 +0800

    router

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -8,7 +8,24 @@ import './App.css'
 // import ReduxPage from './pages/ReduxPage';
 // import HooksPage from './pages/HooksPage';
 // import ReactReduxPage from './pages/ReactReduxPage';
-import ReactReduxHookPage from './pages/ReactReduxHookPage';
+// import ReactReduxHookPage from './pages/ReactReduxHookPage';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useRouteMatch,
+  useHistory,
+  useLocation,
+  useParams,
+  withRouter,
+  Prompt
+} from "react-router-dom";
+import HomePage from './pages/routepages/HomePage';
+import UserPage from './pages/routepages/UserPage';
+import LoginPage from './pages/routepages/LoginPage';
+import _404Page from './pages/routepages/_404Page';
 
 function App() {
   return (
@@ -21,9 +38,30 @@ function App() {
       {/* <ReduxPage/> */}
       {/* <ReactReduxPage/> */}
       {/* <HooksPage/> */}
-      <ReactReduxHookPage/>
+      {/* <ReactReduxHookPage/> */}
+
+
+      <Router>
+        <Link to="/">首页</Link>
+        <Link to="/user">用户中心</Link>
+        <Link to="/login">登录</Link>
+        {/* <Link to="/product/123">商品</Link> */}
+
+        <Switch>
+          <Route exact path="/" children={children} component={HomePage} />
+          <Route path="/user" component={UserPage} />
+          <Route path="/login" component={LoginPage} />
+          {/* <Route path="/product" component={} /> */}
+          <Route component={_404Page} />
+        </Switch>
+      </Router>
     </div>
   );
+}
+
+
+function children(props) {
+  return <div>children</div>
 }
 
 export default App;
